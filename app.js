2c779546651ec888c97dfbf75fa3689e1f14e031
Micky Awelachew-Yirga <mickyawelachew@yahoo.com>
2025-04-05 21:09:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-import { registerServiceWorker } from './sw-register.js';
-registerServiceWorker();
-console.log("todo.js is running...");
+// app.js
+
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(() => console.log('Service Worker Registered'))
+      .catch(err => console.log('Service Worker Registration Failed', err));
+  });
+}
+
+// Your normal code
+console.log("App.js is working...");
