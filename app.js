13fac5b30efcbf6fe04981fc41cf29243fbd07e1
Micky Awelachew-Yirga <mickyawelachew@yahoo.com>
2025-04-05 21:13:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,31 @@ if ('serviceWorker' in navigator) {
   });
 }
 console.log("App.js is working...");
+
+let deferredPrompt;
+const installBtn = document.getElementById('install-btn');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+
+  e.preventDefault();
+  deferredPrompt = e;
+  
+  installBtn.style.display = 'block';
+
+  installBtn.addEventListener('click', () => {
+    // Hide the install button
+    installBtn.style.display = 'none';
+
+    deferredPrompt.prompt();
+
+    deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the install prompt');
+        } else {
+          console.log('User dismissed the install prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+});
